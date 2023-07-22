1e348c2f9c5ac2d9dd8a29ae8851d716128f199a
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-07-22 10:56:22 +0530

    Add project files.

---
+++ HelloCherno.cpp
@@ -0,0 +1,12 @@
+﻿// HelloCherno.cpp : Defines the entry point for the application.
+//
+
+#include "HelloCherno.h"
+
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::cin.get();
+	return 0;
+}
